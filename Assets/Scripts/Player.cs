ffeaa65810b960bef524be995818c7876641e1a2
Zihan Yang <ws704603301@gmail.com>
2024-03-11 17:37:08 -0400

    Add boundaries to confine the palyer's movement within the screen

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,16 +6,39 @@ using UnityEngine.InputSystem;
 public class Player : MonoBehaviour
 {
     [SerializeField] float moveSpeed = 5f;
+    [SerializeField] float paddingLeft;
+    [SerializeField] float paddingRight;
+    [SerializeField] float paddingTop;
+    [SerializeField] float paddingBottom;
+
     Vector2 rawInput;
+    Vector2 minBounds;  // for bottomleft corner of the screen
+    Vector2 maxBounds;  // for topright corner of the screen
+
+    void Start()
+    {
+        InitBounds();
+    }
+
     void Update()
     {
         Move();
     }
 
+    void InitBounds()
+    {
+        minBounds = Camera.main.ViewportToWorldPoint(new Vector2(0, 0));
+        maxBounds = Camera.main.ViewportToWorldPoint(new Vector2(1, 1));
+    }
+
     void Move()
     {
         Vector3 delta = rawInput * moveSpeed * Time.deltaTime;  // make movement framerate independent
-        transform.position += delta;
+        Vector2 newPos = new Vector2();
+        // clamp the player position to the screen bounds
+        newPos.x = Mathf.Clamp(transform.position.x + delta.x, minBounds.x + paddingLeft, maxBounds.x - paddingRight);
+        newPos.y = Mathf.Clamp(transform.position.y + delta.y, minBounds.y + paddingBottom, maxBounds.y - paddingTop);
+        transform.position = newPos;
     }
 
     void OnMove(InputValue value)
