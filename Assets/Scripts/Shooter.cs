86cb3aa4d8235309803d39217e624cd9130bc951
Zihan Yang <ws704603301@gmail.com>
2024-03-13 12:38:48 -0400

    Enemies now are able to fire at player

--- Assets/Scripts/Shooter.cs
+++ Assets/Scripts/Shooter.cs
@@ -4,17 +4,32 @@ using UnityEngine;
 
 public class Shooter : MonoBehaviour
 {
+    [Header("General")]
     [SerializeField] GameObject projectilePrefab;
     [SerializeField] float projectileSpeed = 10f;
     [SerializeField] float projectileLifetime = 5f;
-    [SerializeField] float firingRate = 0.1f;
+    [SerializeField] float baseFiringRate = 0.1f;
+
+    [Header("AI")]
+    [SerializeField] float firingRateVariance = 0f;
+    [SerializeField] float minFiringRate = 0.1f;
+    [SerializeField] bool isAI = false;
 
     Coroutine firingCoroutine;
 
+    [HideInInspector]
     public bool isFiring;
+
     void Start()
     {
-
+        if (isAI)
+        {
+            isFiring = true;
+        }
+        else
+        {
+            isFiring = false;
+        }
     }
 
     void Update()
@@ -44,12 +59,15 @@ public class Shooter : MonoBehaviour
             Rigidbody2D laserRigidbody = laser.GetComponent<Rigidbody2D>();
             if (laserRigidbody != null)
             {
-                laserRigidbody.velocity = new Vector2(0, 1) * projectileSpeed;  // move the laser up (in the y direction
-                // laserRigidbody.velocity = transform.up * projectileSpeed;  // alternative way to move the laser up
+                // laserRigidbody.velocity = new Vector2(0, 1) * projectileSpeed;  // move the laser up (in the y direction
+                laserRigidbody.velocity = transform.up * projectileSpeed;  // alternative way to move the laser up
             }
 
             Destroy(laser, projectileLifetime);
-            yield return new WaitForSeconds(firingRate);  // wait for the firing rate (in seconds
+
+            float firingRate = baseFiringRate + Random.Range(-firingRateVariance, firingRateVariance);  // add some randomness to the firing rate (in seconds)
+            firingRate = Mathf.Clamp(firingRate, minFiringRate, float.MaxValue);  // make sure the firing rate is not less than the minimum firing rate (in seconds
+            yield return new WaitForSeconds(firingRate); // wait for the firing rate (in seconds)
         }
     }
 }
