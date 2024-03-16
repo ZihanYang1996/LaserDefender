16a8aced27f31b5db6966d9403cd483caadfe349
Zihan Yang <ws704603301@gmail.com>
2024-03-16 01:13:29 -0400

    AudioPlayer fixe by adding a lock to avoid two coroutine modify same AudioSource at same time, maybe there's a better way, will think about it. Besides, the rotational shake doesn't seems too good, it always goes to the same direction, will try to have it fixed

--- Assets/Scripts/AudioPlayer.cs
+++ Assets/Scripts/AudioPlayer.cs
@@ -7,11 +7,11 @@ public class AudioPlayer : MonoBehaviour
 {
     [Header("Shooting")]
     [SerializeField] AudioClip shootingClip;
-    [SerializeField] [Range(0f, 1f)] float shootingVolume = 0.5f;
+    [SerializeField][Range(0f, 1f)] float shootingVolume = 0.5f;
 
     [Header("Explosion")]
     [SerializeField] AudioClip explosionClip;
-    [SerializeField] [Range(0f, 1f)] float explosionVolume = 0.5f;
+    [SerializeField][Range(0f, 1f)] float explosionVolume = 0.5f;
 
     [Header("Music")]
     [SerializeField] AudioClip menuMusic;
@@ -19,8 +19,16 @@ public class AudioPlayer : MonoBehaviour
     [SerializeField] AudioClip gameOverMusic;
     [SerializeField] float fadeOutTime = 1f;
     [SerializeField] float fadeInTime = 1f;
+    [SerializeField][Range(0f, 1f)] float musicVolume = 0.5f;
 
     public static AudioPlayer instance { get; private set; }
+
+    AudioSource audioSourceA;
+    AudioSource audioSourceB;
+
+    Dictionary<AudioSource, bool> audioSourceLocks = new Dictionary<AudioSource, bool>();
+
+    bool isPlayingA = false;
 
     void Awake()
     {
@@ -34,6 +42,40 @@ public class AudioPlayer : MonoBehaviour
             gameObject.SetActive(false);
             Destroy(gameObject);
         }
+
+        GameObject subAudioPlayerA = new GameObject("subAudioPlayerA");
+        subAudioPlayerA.transform.parent = transform;  // Set the parent of the subAudioPlayerA to the AudioPlayer
+
+        GameObject subAudioPlayerB = new GameObject("subAudioPlayerB");
+        subAudioPlayerB.transform.parent = transform;  // Set the parent of the subAudioPlayerB to the AudioPlayer
+
+        // Create two audio sources
+        audioSourceA = subAudioPlayerA.AddComponent<AudioSource>();
+        audioSourceA.volume = musicVolume;
+        audioSourceA.playOnAwake = false;
+        audioSourceA.loop = true;
+
+        audioSourceB = subAudioPlayerB.AddComponent<AudioSource>();
+        audioSourceB.volume = 0;
+        audioSourceB.playOnAwake = false;
+        audioSourceB.loop = true;
+
+        audioSourceLocks.Add(audioSourceA, false);
+        audioSourceLocks.Add(audioSourceB, false);
+
+    }
+
+    void Update()
+    {
+        // Adjust the volume of the audio sources
+        // if (isPlayingA && audioSourceA.volume != musicVolume)
+        // {
+        //     audioSourceA.volume = musicVolume;
+        // }
+        // else if (!isPlayingA && audioSourceB.volume != musicVolume)
+        // {
+        //     audioSourceB.volume = musicVolume;
+        // }
     }
 
     public void PlayShootingClip()
@@ -87,51 +129,68 @@ public class AudioPlayer : MonoBehaviour
 
     private void switchMusic(AudioClip music)
     {
-        AudioSource audioSource = gameObject.GetComponent<AudioSource>();
-        float startVolume = audioSource.volume;
-
-        StartCoroutine(FadeOut(audioSource, fadeOutTime));
-        StartCoroutine(FadeIn(music, startVolume, fadeInTime));
+        if (isPlayingA)
+        {
+            isPlayingA = false;
+            StartCoroutine(FadeIn(audioSourceB, music, musicVolume, fadeInTime));
+            StartCoroutine(FadeOut(audioSourceA, fadeOutTime));
+        }
+        else
+        {
+            isPlayingA = true;
+            StartCoroutine(FadeIn(audioSourceA, music, musicVolume, fadeInTime));
+            StartCoroutine(FadeOut(audioSourceB, fadeOutTime));
+        }
     }
 
     IEnumerator FadeOut(AudioSource audioSource, float fadeTime)
-    {   
-        float startVolume = audioSource.volume;
+    {
+        while (audioSourceLocks[audioSource])
 
-        while (audioSource.volume > 0)
         {
-            audioSource.volume -= startVolume * Time.deltaTime / fadeTime;
+            Debug.Log("Fade Out Waiting");
             yield return null;
         }
+        audioSourceLocks[audioSource] = true; // lock the audio source
+        Debug.Log("Fading Out Locked");
 
-        audioSource.Stop();
-        Destroy(audioSource);
-    }
-
-    IEnumerator FadeIn(AudioClip music, float originVolume, float fadeTime)
-    {
-        AudioSource audioSource = gameObject.AddComponent<AudioSource>();
-        audioSource.volume = 0;
-        audioSource.loop = true;
-
-        audioSource.clip = music;
-        audioSource.Play();
-
-        // while (audioSource.volume < originVolume)
-        // {
-        //     audioSource.volume += originVolume * Time.deltaTime / fadeTime;
-        //     yield return null;
-        // }
-
-        // Alternative way using Lerp
+        float startVolume = audioSource.volume;
         float time = 0;
+
         while (time < fadeTime)
         {
-            audioSource.volume = Mathf.Lerp(0, originVolume, time / fadeTime);
+            audioSource.volume = Mathf.Lerp(startVolume, 0, time / fadeTime);
             time += Time.deltaTime;
             yield return null;
         }
 
-        audioSource.volume = originVolume;  // ensure the volume is set to the original value
+        audioSource.Stop();
+        audioSourceLocks[audioSource] = false;  // unlock the audio source
+        Debug.Log("Fading Out Unlocked");
+    }
+
+    IEnumerator FadeIn(AudioSource audioSource, AudioClip music, float targetVolumn, float fadeTime)
+    {
+        while (audioSourceLocks[audioSource])
+        {
+            Debug.Log("Fade In Waiting");
+            yield return null;
+        }
+        audioSourceLocks[audioSource] = true; // lock the audio source
+        Debug.Log("Fading In Locked");
+        audioSource.clip = music;
+        audioSource.Play();
+
+        float time = 0;
+        while (time < fadeTime)
+        {
+            audioSource.volume = Mathf.Lerp(0, targetVolumn, time / fadeTime);
+            time += Time.deltaTime;
+            yield return null;
+        }
+
+        audioSource.volume = targetVolumn;  // ensure the volume is set to the original value
+        audioSourceLocks[audioSource] = false;  // unlock the audio source
+        Debug.Log("Fading In Unlocked");
     }
 }
